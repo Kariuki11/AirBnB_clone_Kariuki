29645d3c4d189966c6c40086df80b19de9df9cbb
Kariuki11 <irungukenneth433@gmail.com>
2024-04-16 14:38:49 +0300

    The web flask folder

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Flask"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a given string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return a given string"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cText(text):
+    """displays C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonText(text="is cool"):
+    """displays Python followed by value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isNumber(n):
+    """displays “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n=None):
+    """displays HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n=None):
+    """displays a HTML page only if n is an integer:
+    H1 tag: “Number: n is even|odd” inside the tag BODY
+    """
+    if isinstance(n, int):
+        if n % 2:
+            eo = "odd"
+        else:
+            eo = "even"
+        return render_template("6-number_odd_or_even.html", n=n, eo=eo)
+
+
+if __name__ == "__main__":
+    """Start Flask web application"""
+    app.run()
---
+++ web_flask/README.md
@@ -0,0 +1,13 @@
+<h1>0x04. AirBnB_clone_v2- Web framework</h1>
+<h2>Objectives</h2>
+<ol>
+<li>What is a Web Framework</li>
+<li>How to define routes in Flask</li>
+<li>How to build a web framework with Flask</li>
+<li>What is a route</li>
+<li>What is a template</li>
+<li>How to handle variable in a route</li>
+<li>How to create the HTML response in Flask by using a template</li>
+<li>How to create a dynamic template (loops, conditions…)</li>
+<li>How to display in the HTML data from a MySQL database</li>
+</ol>
